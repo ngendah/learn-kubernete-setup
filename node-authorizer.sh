c1987f434f638ac8c976855d05bac41ebfd4bba1
Ngenda Henry <ngendahk@gmail.com>
2022-11-15 17:25:47 +0300

    Annotate files with a index to order them how they are to be executed

--- node-authorizer.sh
+++
@@ -1,5 +0,0 @@
-sudo mv -v ~/worker-1.key ~/worker-1.crt /var/lib/kubernetes/pki/
-sudo mv -v ~/worker-1.kubeconfig /var/lib/kubernetes/
-sudo chown -Rv root:root /var/lib/kubernetes/pki
-sudo chmod -Rv 600 /var/lib/kubernetes/pki
-sudo chmod -v 600 /var/lib/kubernetes/worker-1.kubeconfig
