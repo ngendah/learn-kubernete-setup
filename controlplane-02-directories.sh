3786344e45f68328cb0726f9775ae60d2b6dbd76
Ngenda Henry <ngendahk@gmail.com>
2022-12-06 15:56:27 +0300

    Update directory creation

--- controlplane-02-directories.sh
+++ controlplane-02-directories.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # create necessary directories
-sudo mkdir -vp $(jq -r ".nodes.control_plane.paths[]" cluster-config.json)
+sudo mkdir -vp $(jq -r ".nodes.control_plane.kubernetes.paths[]" cluster-config.json)
+sudo mkdir -vp $(jq -r ".nodes.control_plane.etcd.paths[]" cluster-config.json)
