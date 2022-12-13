30d459f68cfb4807df2d16ccec66733fc3aecf00
Ngenda Henry <ngendahk@gmail.com>
2022-12-13 17:00:49 +0300

    Fix data cache directory owner to current user

--- controlplane-02-directories.sh
+++ controlplane-02-directories.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Directories
-sudo mkdir -vp $(jq -r ".script_data_dir" cluster-config.json)
+mkdir -vp $(jq -r ".script_data_dir" cluster-config.json)
 sudo mkdir -vp $(jq -r ".nodes.control_plane.kubernetes.paths[]" cluster-config.json)
 sudo mkdir -vp $(jq -r ".nodes.control_plane.etcd.paths[]" cluster-config.json)
