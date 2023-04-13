0a5ca77cee053fd2937eb1b35af4ea0c2601d45c
Ngenda Henry <ngendahk@gmail.com>
2023-04-13 14:34:06 +0300

    Add download dir to controlplane kubectl

--- k8s-scripts/controlplane-01-kubectl.sh
+++ k8s-scripts/controlplane-01-kubectl.sh
@@ -9,39 +9,45 @@
 # shellcheck disable=SC2086
 source common.sh
 
-kubectl_download() {
-  FILE_NAME=kubectl
-  if [ ! -f "$DATA_DIR/$FILE_NAME" ]; then
+KUBECTL_FILE_NAME=kubectl
+KUBECTL_SETUP_DIR=$DATA_DIR/$KUBECTL_FILE_NAME
+
+kubectl_create_setup_dirs() {
+  mkdir -p $KUBECTL_SETUP_DIR
+}
+
+kubectl_download() { 
+  if [ ! -f "$KUBECTL_SETUP_DIR/$KUBECTL_FILE_NAME" ]; then
     wget --show-progress --https-only --timestamping \
-      "https://storage.googleapis.com/kubernetes-release/release/$KUBERNETES_VERSION/bin/linux/amd64/$FILE_NAME"
-    mv $FILE_NAME $DATA_DIR/
+      "https://storage.googleapis.com/kubernetes-release/release/$KUBERNETES_VERSION/bin/linux/amd64/$KUBECTL_FILE_NAME"
+    mv $KUBECTL_FILE_NAME $KUBECTL_SETUP_DIR/
   fi
 }
 
 kubectl_generate() {
   master_check_dirs_and_create
-
+  kubectl_create_setup_dirs
   kubectl_download
 }
 
 kubectl_install() {
-  sudo cp -v $DATA_DIR/kubectl $BIN_DIR
+  sudo cp -v $KUBECTL_SETUP_DIR/$KUBECTL_FILE_NAME $BIN_DIR
 
-  sudo chown -v $USER:root $BIN_DIR/kubectl
-  sudo chmod -v 500 $BIN_DIR/kubectl
+  sudo chown -v $USER:root $BIN_DIR/$KUBECTL_FILE_NAME
+  sudo chmod -v 500 $BIN_DIR/$KUBECTL_FILE_NAME
 }
 
 kubectl_remove(){
-  sudo rm -f $BIN_DIR/kubectl
+  sudo rm -f $BIN_DIR/$KUBECTL_FILE_NAME
 }
 
 kubectl_remove_all(){
   kubectl_remove
-  rm -f $DATA_DIR/kubectl
+  rm -fr $KUBECTL_SETUP_DIR/*
 }
 
 kubectl_reinstall(){
-  if [ -f $DATA_DIR/kubectl ]; then
+  if [ -f $KUBECTL_SETUP_DIR/$KUBECTL_FILE_NAME ]; then
     kubectl_remove
     kubectl_install
   else
