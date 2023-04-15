8a989908903ca9527b933239ff3ab7354ee3af72
Ngenda Henry <ngendahk@gmail.com>
2023-04-15 13:14:29 +0300

    Add setup dir for worker kube-proxy

--- k8s-scripts/worker-04-kube-proxy.sh
+++ k8s-scripts/worker-04-kube-proxy.sh
@@ -12,21 +12,68 @@ source common.sh
 # shellcheck disable=SC2155
 export NODE_HOSTNAME=$(ssh $NODE sudo hostname -s)
 
-kube_proxy_generate() {
-  master_ca_exists
+KUBE_PROXY_FILE_NAME=kube-proxy
+KUBE_PROXY_SETUP_DIR_NAME=kube-proxy
+KUBE_PROXY_SETUP_DIR="${DATA_DIR}/$KUBE_PROXY_SETUP_DIR_NAME"
 
-  openssl genrsa -out $DATA_DIR/kube-proxy.key 2048
-  openssl req -new -key $DATA_DIR/kube-proxy.key \
+kube_proxy_download() {
+  PROXY_DOWNLOAD_FILE_NAME=$KUBE_PROXY_SETUP_DIR/$KUBE_PROXY_FILE_NAME
+  if [ ! -f $PROXY_DOWNLOAD_FILE_NAME ]; then
+    wget -P $KUBE_PROXY_SETUP_DIR -q --show-progress --https-only --timestamping \
+      "https://storage.googleapis.com/kubernetes-release/release/$KUBERNETES_VERSION/bin/linux/amd64/$KUBE_PROXY_FILE_NAME"
+  else
+    echo "$PROXY_DOWNLOAD_FILE_NAME already exists, skipping download"
+  fi
+}
+
+kube_proxy_setup_dirs() {
+  master_check_dirs_and_create
+  mkdir -p $KUBE_PROXY_SETUP_DIR
+  ssh -T $NODE sudo mkdir -vp $(jq -r ".nodes.worker.kubernetes.paths[]" cluster-config.json)
+  ssh -T $NODE sudo mkdir -vp $(jq -r ".nodes.worker.kube_proxy.paths[]" cluster-config.json)
+}
+
+kube_proxy_copy_ca_certs() {
+  cp $MASTER_CERT_DIR/$CA_FILE_NAME.crt $MASTER_CERT_DIR/$CA_FILE_NAME.key $KUBE_PROXY_SETUP_DIR
+}
+
+kube_proxy_generate() {
+  kube_proxy_setup_dirs
+  master_ca_exists
+  kube_proxy_copy_ca_certs
+  kube_proxy_download
+
+  openssl genrsa -out $KUBE_PROXY_SETUP_DIR/kube-proxy.key 2048
+  openssl req -new -key $KUBE_PROXY_SETUP_DIR/kube-proxy.key \
     -subj "/CN=system:kube-proxy/O=system:node-proxier" \
-    -out $DATA_DIR/kube-proxy.csr
-  openssl x509 -req -in $DATA_DIR/kube-proxy.csr \
-    -CA $MASTER_CERT_DIR/$CA_FILE_NAME.crt \
-    -CAkey $MASTER_CERT_DIR/$CA_FILE_NAME.key \
+    -out $KUBE_PROXY_SETUP_DIR/kube-proxy.csr
+  openssl x509 -req -in $KUBE_PROXY_SETUP_DIR/kube-proxy.csr \
+    -CA $KUBE_PROXY_SETUP_DIR/$CA_FILE_NAME.crt \
+    -CAkey $KUBE_PROXY_SETUP_DIR/$CA_FILE_NAME.key \
     -CAcreateserial \
-    -out $DATA_DIR/kube-proxy.crt \
+    -out $KUBE_PROXY_SETUP_DIR/kube-proxy.crt \
     -days 1000
 
-  cat <<EOF | tee $DATA_DIR/kube-proxy-config.yaml
+  kubectl config set-cluster $CLUSTER_NAME \
+    --certificate-authority=$KUBE_PROXY_SETUP_DIR/$CA_FILE_NAME.crt \
+    --server=https://$MASTER_1:6443 \
+    --embed-certs=true \
+    --kubeconfig=$KUBE_PROXY_SETUP_DIR/kube-proxy.kubeconfig
+
+  kubectl config set-credentials system:kube-proxy \
+    --client-certificate=$KUBE_PROXY_SETUP_DIR/kube-proxy.crt \
+    --client-key=$KUBE_PROXY_SETUP_DIR/kube-proxy.key \
+    --embed-certs=true \
+    --kubeconfig=$KUBE_PROXY_SETUP_DIR/kube-proxy.kubeconfig
+
+  kubectl config set-context default \
+    --cluster=$CLUSTER_NAME \
+    --user=system:kube-proxy \
+    --kubeconfig=$KUBE_PROXY_SETUP_DIR/kube-proxy.kubeconfig
+
+  kubectl config use-context default --kubeconfig=$KUBE_PROXY_SETUP_DIR/kube-proxy.kubeconfig
+
+  cat <<EOF | tee $KUBE_PROXY_SETUP_DIR/kube-proxy-config.yaml
 kind: KubeProxyConfiguration
 apiVersion: kubeproxy.config.k8s.io/v1alpha1
 clientConnection:
@@ -35,7 +82,7 @@ mode: "iptables"
 clusterCIDR: ${POD_CIDR}
 EOF
 
-  cat <<EOF | tee $DATA_DIR/kube-proxy.service
+  cat <<EOF | tee $KUBE_PROXY_SETUP_DIR/kube-proxy.service
 [Unit]
 Description=Kubernetes Kube Proxy
 Documentation=https://github.com/kubernetes/kubernetes
@@ -52,56 +99,34 @@ EOF
 }
 
 kube_proxy_install() {
+  scp -r $KUBE_PROXY_SETUP_DIR $NODE:~
+
   cat <<EOF | ssh -T $NODE
-echo "Downloading kube-proxy-$KUBERNETES_VERSION"
-wget -q --https-only --timestamping https://storage.googleapis.com/kubernetes-release/release/KUBERNETES_VERSION/bin/linux/amd64/kube-proxy
-sudo mv -v ./kube-proxy $BIN_DIR
+sudo cp -v ~/$KUBE_PROXY_SETUP_DIR_NAME/kube-proxy $BIN_DIR
 
 sudo chown -v root:root $BIN_DIR/kube-proxy
 sudo chmod -v 500 $BIN_DIR/kube-proxy
 EOF
 
-  scp $MASTER_CERT_DIR/$CA_FILE_NAME.crt $DATA_DIR/kube-proxy.key \
-    $DATA_DIR/kube-proxy.crt \
-    $DATA_DIR/kube-proxy-config.yaml \
-    $DATA_DIR/kube-proxy.service \
-    $NODE:~
-
   cat <<EOF | ssh -T $NODE
-kubectl config set-cluster $CLUSTER_NAME \
-    --certificate-authority=$WORKER_CERT_DIR/$CA_FILE_NAME.crt \
-    --server=https://$MASTER_1:6443 \
-    --kubeconfig=kube-proxy.kubeconfig
+sudo cp -v ~/$KUBE_PROXY_SETUP_DIR_NAME/$CA_FILE_NAME.crt $WORKER_CERT_DIR
 
-kubectl config set-credentials system:kube-proxy \
-    --client-certificate=$KUBE_PROXY_CERT_DIR/kube-proxy.crt \
-    --client-key=$KUBE_PROXY_CERT_DIR/kube-proxy.key \
-    --kubeconfig=kube-proxy.kubeconfig
-
-kubectl config set-context default \
-    --cluster=$CLUSTER_NAME \
-    --user=system:kube-proxy \
-    --kubeconfig=kube-proxy.kubeconfig
-
-kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
-
-sudo mv -v ~/kube-proxy.kubeconfig $KUBE_PROXY_CONFIG_DIR
-
-sudo chown -Rv root:root $KUBE_PROXY_KUBE_PROXY_CONFIG_DIR
-sudo chmod -Rv 600 $KUBE_PROXY_KUBE_PROXY_CONFIG_DIR
-EOF
-
-  cat <<EOF | ssh -T $NODE
-sudo mv -v ~/$CA_FILE_NAME.crt $WORKER_CERT_DIR
-
-sudo mv -v ~/kube-proxy.key ~/kube-proxy.crt $KUBE_PROXY_CERT_DIR
-sudo mv -v ~/kube-proxy-config.yaml $KUBE_PROXY_CONFIG_DIR
-sudo mv -v ~/kube-proxy.service $SERVICES_DIR
+sudo cp -v ~/$KUBE_PROXY_SETUP_DIR_NAME/kube-proxy.key ~/$KUBE_PROXY_SETUP_DIR_NAME/kube-proxy.crt $KUBE_PROXY_CERT_DIR
+sudo cp -v ~/$KUBE_PROXY_SETUP_DIR_NAME/kube-proxy-config.yaml $KUBE_PROXY_CONFIG_DIR
+sudo cp -v ~/$KUBE_PROXY_SETUP_DIR_NAME/kube-proxy.service $SERVICES_DIR
 
 sudo chown -Rv root:root $KUBE_PROXY_CERT_DIR
 sudo chmod -Rv 600 $KUBE_PROXY_CERT_DIR
+
 sudo chown -v root:root $SERVICES_DIR/kube-proxy.service
 sudo chmod -v 600 $SERVICES_DIR/kube-proxy.service
+EOF
+
+  cat <<EOF | ssh -T $NODE
+sudo cp -v ~/$KUBE_PROXY_SETUP_DIR_NAME/kube-proxy.kubeconfig $KUBE_PROXY_CONFIG_DIR
+
+sudo chown -Rv root:root $KUBE_PROXY_CONFIG_DIR
+sudo chmod -Rv 600 $KUBE_PROXY_CONFIG_DIR
 EOF
 }
 
@@ -120,7 +145,7 @@ EOF
 
 kube_proxy_remove_all() {
   kube_proxy_remove
-  rm -fr $DATA_DIR/kube-proxy*
+  rm -fr $KUBE_PROXY_SETUP_DIR/*
 }
 
 kube_proxy_start() {
@@ -143,29 +168,43 @@ kube_proxy_restart() {
 }
 
 kube_proxy_reinstall() {
-  kube_proxy_remove_all
+  kube_proxy_remove
   kube_proxy_generate
   kube_proxy_install
 }
 
 case $1 in
-"remove") ;;
-
-"generate") ;;
-
-"install") ;;
-
+"setup-dirs")
+  kube_proxy_setup_dirs
+  ;;
+"download")
+  kube_proxy_setup_dirs
+  kube_proxy_download
+  ;;
+"remove")
+  kube_proxy_stop
+  kube_proxy_remove
+  ;;
+"generate")
+  kube_proxy_generate
+  ;;
+"install")
+  kube_proxy_stop
+  kube_proxy_install
+  kube_proxy_start
+  ;;
 "reinstall")
   kube_proxy_stop
   kube_proxy_reinstall
   kube_proxy_start
   ;;
-"remove-all") ;;
-
+"remove-all")
+  kube_proxy_stop
+  kube_proxy_remove_all
+  ;;
 "stop")
   kube_proxy_stop
   ;;
-
 "start")
   kube_proxy_start
   ;;
